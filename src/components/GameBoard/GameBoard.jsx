7e85715a9e1d734264b3322e043c7632c26ccbd1
Alex Lopez <coquixo98@gmail.com>
2023-02-06 17:33:58 +0100

    making pieces logic

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import EmptySpace from "../GamePieces/EmptySpace/EmptySpace";
+import King from "../GamePieces/King/King";
 import "./GameBoard.scss";
 
 const GameBoard = () => {
+
+    const [pieceValue, setPieceValue] = useState("empty")
+
     const cardinalY = 8;
     const cardinalX = 8;
     let board = [];
 
+    const movePiece = (i, j) => {
+        console.log("Has clickado la ficha x, y aqui va la funcion de moverlo: ")
+        console.log(`posicion: ${i}${j}`)
+    }
+
     for (let i = 0; i < cardinalX; i++) {
         for (let j = 0; j < cardinalY; j++) {
             board.push(
-                <span className="inGameSingleBox">
-                    {i + 1}
-                    {j + 1}
+                <span className="inGameSingleBox" onClick={() => movePiece(i, j)}>
+
+                    <EmptySpace />
                 </span>
             );
         }
---
+++ src/components/GamePieces/EmptySpace/EmptySpace.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./EmptySpace.scss"
+
+const EmptySpace = () => {
+    return (
+        <div className="emptySpaceDesign">
+            {" "}
+        </div>
+    )
+}
+
+export default EmptySpace
