2435917e20de5b1f56c97c53ea9935b3be9e4c3c
Alex Lopez <coquixo98@gmail.com>
2023-02-06 18:04:52 +0100

    testing with props

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -5,24 +5,43 @@ import King from "../GamePieces/King/King";
 import "./GameBoard.scss";
 
 const GameBoard = () => {
-
-    const [pieceValue, setPieceValue] = useState("empty")
+    const [pieceValue, setPieceValue] = useState({
+        king: 1,
+        queen: 2,
+        rook: 3,
+    });
 
     const cardinalY = 8;
     const cardinalX = 8;
     let board = [];
 
     const movePiece = (i, j) => {
-        console.log("Has clickado la ficha x, y aqui va la funcion de moverlo: ")
-        console.log(`posicion: ${i}${j}`)
-    }
+        console.log("Has clickado la ficha x, y aqui va la funcion de moverlo: ");
+        console.log(`posicion: ${i}${j}`);
+        console.log(`pieceValue`);
+    };
+
+
+    const checkWhatComponentIsLoaded = () => {
+        board.forEach((key) => {
+            console.log(key.key);
+        });
+    };
+
+    const spaceClicked = (i, j) => {
+        movePiece(i, j);
+        checkWhatComponentIsLoaded();
+    };
 
     for (let i = 0; i < cardinalX; i++) {
         for (let j = 0; j < cardinalY; j++) {
             board.push(
-                <span className="inGameSingleBox" onClick={() => movePiece(i, j)}>
-
-                    <EmptySpace />
+                <span
+                    key={`${i}${j}`}
+                    className="inGameSingleBox"
+                    onClick={() => spaceClicked(i, j)}>
+                    {i} {j}
+                    {/* <EmptySpace /> */}
                 </span>
             );
         }
@@ -30,9 +49,7 @@ const GameBoard = () => {
 
     return (
         <div className="boardBox">
-            <div className="boardGame">
-                {board}
-            </div>
+            <div className="boardGame">{board}</div>
         </div>
     );
 };
